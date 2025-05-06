d7f5e47b4510919fbfdd03730d6a1fed7a56b644
Adam Tovatt <sporedenstore@hotmail.com>
2025-05-06 16:53:28 +0200

    add comments for lab 3

--- labs/Solutions/Lab3/Algorithms/KruskalSolver.cs
+++ labs/Solutions/Lab3/Algorithms/KruskalSolver.cs
@@ -8,22 +8,26 @@ namespace Lab3.Algorithms
         {
             List<Connection> connections = input.Connections;
 
-            connections.Sort((a, b) => a.Weight.CompareTo(b.Weight));
+            connections.Sort((a, b) => a.Weight.CompareTo(b.Weight)); // sort ascending aka from lowest to highest
 
-            UnionFind unionFind = new UnionFind(input.NumberOfPeople);
+            UnionFind unionFind = new UnionFind(input.NumberOfPeople); // initialize a new instance of the union find class, in this, each person is their own "tree"
 
             int totalWeight = 0;
             int edgesUsed = 0;
 
             foreach (Connection connection in connections)
             {
-                if (unionFind.Union(connection.PersonA, connection.PersonB))
+                // will try to union the nodes / persons (whaever you want to call it) so they are considered part of the same tree
+                // this means that the trees that they are part of will end up with the same root
+                bool didUnion = unionFind.Union(connection.PersonA, connection.PersonB); // if they were already part of the same tree this will return false
+
+                if (didUnion) // if we actually changed anything and now connected two previously not connected trees let's...
                 {
-                    totalWeight += connection.Weight;
-                    edgesUsed++;
+                    totalWeight += connection.Weight; // increase the total tree weight with the weight that was added to it
+                    edgesUsed++; // remember that we used one edge more in this total tree we're building
 
                     if (edgesUsed == input.NumberOfPeople - 1)
-                        break;
+                        break; // we should've gone through all the edges at this point
                 }
             }
 
--- labs/Solutions/Lab3/Algorithms/UnionFind.cs
+++ labs/Solutions/Lab3/Algorithms/UnionFind.cs
@@ -2,49 +2,60 @@
 {
     public class UnionFind
     {
-        private readonly int[] _parent;
-        private readonly int[] _rank;
+        // contains the id's of parents for a certain other id, a bit confusing, but it's like the index is the key and the value at that index is the value
+        private readonly int[] _parent; // parent[i] is the parent of node i in the union-find structure
+
+        // contains the rank (depth of a tree) that has the root that is on a given index in this array, again like the index is the key and the value at that index is the value
+        private readonly int[] _rank;   // so rank[i] is an estimation of depth that is relevant (only) if the node with id/index i is a root node
 
         public UnionFind(int size)
         {
-            _parent = new int[size + 1];
+            _parent = new int[size + 1]; // +1 to use 1-based indexing (people are numbered from 1)
             _rank = new int[size + 1];
 
             for (int i = 1; i <= size; i++)
-                _parent[i] = i;
+                _parent[i] = i; // initially, each node is its own parent
         }
 
-        public int Find(int x)
+        public int FindRoot(int id) // finds the root of a person with the provided id
         {
-            if (_parent[x] != x)
-                _parent[x] = Find(_parent[x]);
+            if (_parent[id] != id)
+                _parent[id] = FindRoot(_parent[id]); // recursively call FindRoot again with the closest parent untill we hit the root
 
-            return _parent[x];
+            return _parent[id];
         }
 
-        public bool Union(int x, int y)
+        public bool Union(int idA, int idB) // will set two sets as being connected
         {
-            int rootX = Find(x);
-            int rootY = Find(y);
+            int rootA = FindRoot(idA); // find root of the person with idA
+            int rootB = FindRoot(idB); // find root of the person with idB
 
-            if (rootX == rootY)
-                return false;
+            if (rootA == rootB)
+                return false; // already connected
 
-            if (_rank[rootX] < _rank[rootY])
+            if (_rank[rootA] < _rank[rootB])
             {
-                _parent[rootX] = rootY;
+                _parent[rootA] = rootB; // attach smaller tree under larger tree
             }
-            else if (_rank[rootX] > _rank[rootY])
+            else if (_rank[rootA] > _rank[rootB])
             {
-                _parent[rootY] = rootX;
+                _parent[rootB] = rootA;
             }
             else
             {
-                _parent[rootY] = rootX;
-                _rank[rootX]++;
+                _parent[rootB] = rootA;
+                _rank[rootA]++; // increase rank only when ranks are equal
+
+                // the reason we only increment the rank when the trees have equal ranks is because then 
+                // one of the trees will unavoidably increase one in rank as it gets a new parent
+
+                // if the trees are not of equal rank we add the smaller tree under the larger tree which means
+                // that the rank doesn't change since it represents the max depth and the larger tree is
+                // the one with max depth of the two before the union as well as after the union
+                // meaning that the rank remains unchanged
             }
 
-            return true;
+            return true; // union successful
         }
     }
 }
